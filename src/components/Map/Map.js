d3af8a74111ac4bc3131eb98d9b1040227286d4a
Momoh Abdulsamad <samadmomoh@gmail.com>
2022-09-16 19:05:58 +0100

    Update

--- src/components/Map/Map.js
+++ src/components/Map/Map.js
@@ -1,7 +1,29 @@
-import React from 'react';
+import GoogleMapReact from 'google-map-react';
+import { Paper, Typography, useMediaQuery } from '@material-ui/core';
+import { LocationOnOutlined } from '@material-ui/icons';
+import { Rating } from '@material-ui/lab';
+
+import useStyles from './styles';
 
 const Map = () => {
-  return <div>Map</div>;
+  const classes = useStyles();
+  // const isMobile = useMediaQuery();
+  const coordinates = { lat: 0, lng: 0 };
+
+  return (
+    <div className={classes.mapContainer}>
+      <GoogleMapReact
+        bootstrapURLKeys={{ key: 'AIzaSyA4SL16bRvSnogiDhZ_BmtU_PiqCsVPsRY' }}
+        defaultCenter={coordinates}
+        center={coordinates}
+        defaultZoom={14}
+        margin={[50, 50, 50, 50]}
+        options={''}
+        onChange={''}
+        onChildClick={''}
+      ></GoogleMapReact>
+    </div>
+  );
 };
 
 export default Map;
